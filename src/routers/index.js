2cbc6b45f4f1076cf22cca445ab09a33536f29c0
Berkayergun16 <berkay.ergun16@gmail.com>
2023-02-10 11:46:47 +0300

    views folder added.

--- src/routers/index.js
+++ src/routers/index.js
@@ -3,10 +3,11 @@ import express from 'express';
 
 // Routers
 import taskRouter from './taskRouter.js';
-
+import loginRouter from './loginRouter.js';
 const router = express.Router();
 
 // Routes
+router.use('/', loginRouter)
 router.use('/tasks', taskRouter)
 
 export default router;
